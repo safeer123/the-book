2c5ab16606764815e551060affab010fbf06121b
Safeer Chonengal <safeer2c@gmail.com>
2023-09-02 07:01:35 +0530

    Fixed the home page padding issue

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,25 @@
-import React from "react";
 import { Link, createBrowserRouter, RouterProvider } from "react-router-dom";
 import ApiTest from "../components/api-test";
 import SuraList from "../components/sura-list";
 import WordGame from "../components/word-game";
+import { styled } from "styled-components";
 
+const HomeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin: 36px;
+  position: absolute;
+`
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <ul style={{ fontSize: 24, lineHeight: 2 }}>
-        <li><Link to="/api-test">Home</Link></li>
-        <li><Link to="/suras">Sura List</Link></li>
-      </ul>
+      <HomeWrapper>
+        <div><Link to="/api-test">Home</Link></div>
+        <div><Link to="/suras">Sura List</Link></div>
+      </HomeWrapper>
+
     )
   },
   {
